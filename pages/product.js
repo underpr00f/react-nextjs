656bb8301a2d0d05af53eb33d5f3c506014322c3
underpr00f <underproof2014@gmail.com>
2019-06-09 18:20:39 +0300

    remove redirects

--- pages/product.js
+++ pages/product.js
@@ -1,13 +1,8 @@
-import { PathUtil } from '../utils/PathUtil'
-
 const Product = ({slug}) => {   
-
   return <h1>The product page {slug}</h1>
-
 }
 
 Product.getInitialProps = async function({req, query, pathname}) {
-
   return {
     slug: query.slug
   };
