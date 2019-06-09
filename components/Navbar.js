6f6d0e6e082a4d83ffa80538c87be80be9f3a3b4
underpr00f <underproof2014@gmail.com>
2019-06-09 14:41:04 +0300

    microdata navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,22 +2,22 @@ import Link from 'next/link'
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary" itemScope itemType="http://schema.org/SiteNavigationElement">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="#">Navbar</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
 
       <div className="collapse navbar-collapse" id="navbarColor01">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <Link href="/" passHref><a>Home<span className="sr-only">(current)</span></a></Link>
+        <ul className="navbar-nav mr-auto" itemScope itemType="http://schema.org/SiteNavigationElement">
+          <li className="nav-item active" itemProp="name" role="menuitem">
+            <Link href="/" passHref><a itemProp="url" title="title of hyperlink">Home<span className="sr-only">(current)</span></a></Link>
           </li>
-          <li className="nav-item">
-            <Link href="/about" passHref><a>About</a></Link>
+          <li className="nav-item" itemProp="name" role="menuitem">
+            <Link href="/about" passHref><a itemProp="url" title="title of hyperlink">About</a></Link>
           </li>
-          <li className="nav-item">
-            <Link href="/posts" passHref><a>Post</a></Link>
+          <li className="nav-item" itemProp="name" role="menuitem">
+            <Link href="/posts" passHref><a itemProp="url" title="title of hyperlink">Post</a></Link>
           </li>
         </ul>
 
