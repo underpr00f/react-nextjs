93fd5ba3f36a9c1f12870a0d273d87e075ad8b09
underpr00f <underproof2014@gmail.com>
2019-06-11 03:10:13 +0300

    remove / from urls

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app
 
     server.get('/posts/post/:title', (req, res) => {
       const actualPage = '/posts/post';
-      const queryParams = { title: req.params.title };
+      const queryParams = { title: req.params.title.replace('/', '') };
       app.render(req, res, actualPage, queryParams);      
     });
 
