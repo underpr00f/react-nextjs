ca8e72691946f1dae06313885f013b90544861af
underpr00f <underproof2014@gmail.com>
2019-08-31 02:12:12 +0300

    fix error page

--- pages/_error.js
+++ pages/_error.js
@@ -4,30 +4,28 @@ import { PathUtil } from '../utils/PathUtil'
 
 class Error extends React.Component {
   static getInitialProps({ res, err, errCode, req, pathname }) {
-    const statusCode = res ? res.statusCode : err ? err.statusCode : errCode ? errCode : null
     const pathValues = PathUtil(req, pathname)
 
     return { 
-      ...pathValues,
-      statusCode }
+      ...pathValues
+      }
   }
 
   render() {
     const title = "404 Next.js"
-    const description = statusCode
-            ? `An error ${statusCode} occurred on server`
+            ? `An error 404 occurred on server`
             : 'An error occurred on client'
-    const {protocol, host, pathname, statusCode} = this.props
+    const {protocol, host, pathname} = this.props
     return (
       <Layout
         title={title} 
-        description={description} 
+        description={'404'} 
         protocol={protocol}
         host={host}
         pathname={pathname}>
         <div>
           <h1>{title}</h1>
-          <p>{description}</p>
+          <p>404</p>
         </div>
       </Layout>
     )
